b832245dbaa17912692dd552d776a0228f4f22dc
Pedro Manoel <pedromanoelpyjs@gmail.com>
2025-12-22 20:09:21 -0300

    .

---
+++ exercicios_resolvidos/ex_043.py
@@ -0,0 +1,12 @@
+# 43) Desenvolva um algoritmo que mostre uma contagem regressiva de 30 até 1, 
+# marcando os números que forem divisíveis por 4, exatamente como mostrado abaixo: 
+# 30 29 [28] 27 26 25 [24] 23 22 21 [20] 19 18 17 [16]... 
+
+nums = [x for x in range(30, 0, -1)]
+
+print()
+for num in nums:
+    print(f'{num} ', end='')
+    if num % 4 == 0:
+        print(f'[{num}] ', end='')
+
